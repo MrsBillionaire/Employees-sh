d3cbb8772d04e114d27d57cade8bb036f566e0a2
MrsBillionaire <judithctchinda@gmail.com>
2022-06-16 19:21:03 -0400

    For practice

---
+++ Employees.sh
@@ -0,0 +1,14 @@
+uthor: Judith Tchinda
+#Date: June 15th, 2022
+
+Name="Judith Tchinda"
+Origin=Cameroon
+County="Montgomery county"
+Team="DevOps team"
+Days=Mondays
+echo "Good morning everyone."
+echo "Today I would like to introduce our newest employee $Name"        
+echo "who is originally from $Origin and curently lives in $Cunty."   
+ech "$Name will be workig with the $Team and will also be part of our stand up meetings that take place on $Days."
+roon
+
